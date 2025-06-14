92109aab8fff1a841933a0a7f9a2390ce4a56d8d
Abdelrhman-Dev <58493502+AbdrhmanDev@users.noreply.github.com>
2025-06-14 18:30:18 +0300

    https

--- AirbnbDashboard-finalproject/src/environments/environment.ts
+++ AirbnbDashboard-finalproject/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://fundamental-amitie-ahmedkamal-a550a1ad.koyeb.app', // Replace with your actual API URL
+  apiUrl: 'https://fundamental-amitie-ahmedkamal-a550a1ad.koyeb.app', // Replace with your actual API URL
   pusher: {
     key: '05876014c06c56f892c1', // Replace with your Pusher key
     cluster: 'eu', // Replace with your Pusher cluster (e.g., 'mt1', 'eu', 'ap1')
